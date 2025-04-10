ee3c767ca15e865c3c257ccdafba090372d6e6a8
=PikaNguyen <=nguyenvannhut0704@gmail.com>
2025-04-10 08:59:30 +0700

    add swagger if u not use postman or maybe like that

--- Restaurants/Restaurants.API/Program.cs
+++ Restaurants/Restaurants.API/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
@@ -27,6 +28,13 @@ await seeder.Seed();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 
 app.UseAuthorization();
 
