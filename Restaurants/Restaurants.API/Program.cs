b3c720a7417a7889cde78418a8a4a91ac5866a4a
=PikaNguyen <=nguyenvannhut0704@gmail.com>
2025-04-29 09:27:54 +0700

    extracting presentation layer services

--- Restaurants/Restaurants.API/Program.cs
+++ Restaurants/Restaurants.API/Program.cs
@@ -1,52 +1,17 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.OpenApi.Models;
 using Restaurants.API.Middlewares;
 using Restaurants.Application.Extensions;
 using Restaurants.Domain.Entities;
 using Restaurants.Infrastructure.Extensions;
-using Restaurants.Infrastructure.Persistance;
 using Restaurants.Infrastructure.Seeders;
-using Serilog;
-using Serilog.Events;
+using Restaurants.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer"
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference  = new OpenApiReference {Type = ReferenceType.SecurityScheme,Id = "bearerAuth"}
-            },
-            []
-        }
-    });
-});
-
-builder.Services.AddScoped<ErrorHandlingMiddleware>();
-builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
-
+builder.AddPresentation();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddIdentityApiEndpoints<User>()
-            .AddEntityFrameworkStores<RestaurantDBContext>();
-builder.Host.UseSerilog((context, configuration) =>
-{
-    configuration
-    .ReadFrom.Configuration(context.Configuration);
-});
 
 var app = builder.Build();
 
